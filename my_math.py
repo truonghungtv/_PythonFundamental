5c7c4a72e0e2844c21311dfca09813340ea11f3b
truonghungtv <83547820+truonghungtv@users.noreply.github.com>
2021-07-21 17:33:06 +0700

    baitapvenha1907

---
+++ my_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum(a,b):
+    return a + b
+def sub(a,b):
+    return a- b
+a = 4
+b = 3
+print(sum(a,b))
+print(sub(a,b))
+
+
+# In[ ]:
+
+
+
+
